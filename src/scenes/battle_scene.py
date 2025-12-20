9353954930b5466dd8e0b77c0cda797ba95bd896
Aaron Su <100979056+Repkironca@users.noreply.github.com>
2025-12-20 18:17:56 +0800

    新增兩種怪物、進階攻擊與屬性克制、怪物攻擊對話與機制調整、新增背包互動、新增進化功能

--- src/scenes/battle_scene.py
+++ src/scenes/battle_scene.py
@@ -37,16 +37,36 @@ class BattleLogger: # 我需要一個聊天室
         start_y = self.rect.top + 10
         line_height = self.font.get_height() + 5
         
-        for i, msg in enumerate(self.messages): # emumerate 就是自動幫你依序裝成 tuple (index, msg)
+        for i, msg in enumerate(self.messages):
             txt_surf = self.font.render(msg, True, (255, 255, 255))
             screen.blit(txt_surf, (self.rect.left + 10, start_y + i * line_height))
 
 class BattleScene(Scene):
     def __init__(self, enemy: EnemyTrainer):
         super().__init__()
+
+        self.game_manager = enemy.game_manager
+
         self.enemy = enemy
-        raw_img = resource_manager.get_image("character/phchou.jpg") # 這是周百祥
-        self.enemy_img = pg.transform.scale(raw_img, (403, 269)) # 原始尺寸
+        self.enemy_type = random.choice(["P.H. Chou", "tp6ru4z", "Bill Louis"])
+        self.is_evolved = False
+
+        img_path = None
+        if self.enemy_type == "P.H. Chou":
+            img_path = "character/phchou.jpg" 
+        elif self.enemy_type == "tp6ru4z":
+            img_path = "character/tp6ru4z.png"
+        elif self.enemy_type == "Bill Louis":
+            img_path = "character/Bill_Louis.png"
+
+        raw_img = resource_manager.get_image(img_path) 
+        if self.enemy_type == "P.H. Chou":
+            self.enemy_img = pg.transform.scale(raw_img, (403, 269))
+        elif self.enemy_type == "tp6ru4z":
+            self.enemy_img = pg.transform.scale(raw_img, (269, 269))
+        elif self.enemy_type == "Bill Louis":
+            self.enemy_img = pg.transform.scale(raw_img, (350, 269))
+
 
         self.player_max_hp = 100
         self.player_hp = 100
@@ -58,14 +78,13 @@ class BattleScene(Scene):
         self.font = pg.font.SysFont("Arial", 22, bold=True)
         
         # 聊天室
-        log_w = 800
+        log_w = 850 # 你知盪這個數字我改多久才抓到最佳解嗎 ==
         log_h = 300
         log_x = (GameSettings.SCREEN_WIDTH - log_w) // 6
         log_y = 150
         self.logger = BattleLogger(log_x, log_y, log_w, log_h, self.font)
         
-        # self.logger.log(f"Encountered {self.enemy.classification.name}!")
-        self.logger.log(f"Encountered P.H.Chou!")
+        self.logger.log(f"Encountered {self.enemy_type}!")
         self.logger.log("Battle Start!")
 
         btn_y = GameSettings.SCREEN_HEIGHT - 120
@@ -118,20 +137,162 @@ class BattleScene(Scene):
             on_click = self.run
         )
         
+        # Evolute Him 按鈕
+        self.evolute_button = Button(
+            img_path = "UI/raw/UI_Flat_Button01a_3.png",
+            img_hovered_path = None,
+            x = 10, y = 10, # 左上角
+            width = 150, height = 50,
+            on_click = self.action_evolute
+        )
+
         self.buttons = [
             (self.attack_button, "Attack"),
             (self.roulette_button, "Russian Roulette"),
             (self.daydream_button, "Daydream"),
             (self.item_button, "Item"),
-            (self.run_button, "Run")
+            (self.run_button, "Run"),
         ]
+
+        # 這個是進階攻擊選單
+        self.show_attack_menu = False
+        
+        # 按鈕位置設定，置中顯示在畫面上方
+        atk_w = 350
+        atk_h = 50
+        atk_start_y = GameSettings.SCREEN_HEIGHT // 2 - 150
+        atk_cx = (GameSettings.SCREEN_WIDTH - atk_w) // 2
+        
+        self.btn_survey = Button(
+            img_path = "UI/raw/UI_Flat_Button01a_3.png",
+            img_hovered_path = None,
+            x = atk_cx, y = atk_start_y,
+            width = atk_w, height = atk_h,
+            on_click = lambda: self.specific_attack("Course Evaluation Survey")
+        )
+
+        self.btn_lake = Button(
+            img_path = "UI/raw/UI_Flat_Button01a_3.png",
+            img_hovered_path = None,
+            x = atk_cx, y = atk_start_y + 60,
+            width = atk_w, height = atk_h,
+            on_click = lambda: self.specific_attack("Throw Him Into Lake Success")
+        )
+
+        self.btn_cheat = Button(
+            img_path = "UI/raw/UI_Flat_Button01a_3.png",
+            img_hovered_path = None,
+            x = atk_cx, y = atk_start_y + 120,
+            width = atk_w, height = atk_h,
+            on_click = lambda: self.specific_attack("Ask for CheatGPT in Midterm Exam")
+        )
+        
+        self.attack_menu_buttons = [
+            (self.btn_survey, "Course Evaluation Survey"),
+            (self.btn_lake, "Throw Him Into Lake Success"),
+            (self.btn_cheat, "Ask for CheatGPT in Midterm Exam")
+        ]
+
+        # 背包
+        self.show_bag_menu = False
+        self.bag_item_buttons = [] # 只有可以使用的物品會出現這個按鈕
+        
+        # 記錄一些奇怪的 Buff
+        self.strength_buff = False # 下次攻擊 * 2.5
+        self.defense_buff = False  # 下次受傷 * 0.25
+
+    # 進化
+    def action_evolute(self):
+        if self.is_battle_over: return
+        if self.is_evolved:
+            self.logger.log(f"{self.enemy_type} is already evolved!")
+            return
+
+        self.is_evolved = True
+        self.enemy_hp *= 2
+        self.enemy_max_hp *= 2
+        
+        img_path = None
+        if self.enemy_type == "P.H. Chou":
+            img_path = "character/big_phchou.jpg"
+        elif self.enemy_type == "tp6ru4z":
+            img_path = "character/big_tp6ru4z.png"
+        elif self.enemy_type == "Bill Louis":
+            img_path = "character/big_Bill_Louis.png"
+            
+        raw_img = resource_manager.get_image(img_path)
+        if self.enemy_type == "P.H. Chou":
+            self.enemy_img = pg.transform.scale(raw_img, (348, 269))
+        elif self.enemy_type == "tp6ru4z":
+            self.enemy_img = pg.transform.scale(raw_img, (201, 269))
+        elif self.enemy_type == "Bill Louis":
+            self.enemy_img = pg.transform.scale(raw_img, (269, 269))
+
+        self.logger.log(f"{self.enemy_type} has EVOLVED! Stats doubled!")
+        sound_manager.play_sound("gugugaga_2.mp3")
 
     def attack(self):
         if self.is_battle_over: return
+        self.show_attack_menu = True
+        self.show_bag_menu = False
+
+    def specific_attack(self, attack_name: str):
+        if self.is_battle_over: return
+        self.show_attack_menu = False
         
-        dmg = random.randint(15, 30)
-        self.enemy_hp -= dmg
-        self.logger.log(f"You attacked! Dealt {dmg} damage.")
+        base_dmg = random.randint(15, 30)
+        multiplier = 1.0
+        if self.strength_buff:
+            multiplier = 2.5
+            self.strength_buff = False
+        
+        if self.enemy_type == "P.H. Chou":
+            if attack_name == "Course Evaluation Survey": multiplier = 1.5
+            elif attack_name == "Ask for CheatGPT in Midterm Exam": multiplier = 0.5
+            
+        elif self.enemy_type == "tp6ru4z":
+            if attack_name == "Throw Him Into Lake Success": multiplier = 1.5
+            elif attack_name == "Course Evaluation Survey": multiplier = 0.5
+            
+        elif self.enemy_type == "Bill Louis":
+            if attack_name == "Ask for CheatGPT in Midterm Exam": multiplier = 1.5
+            elif attack_name == "Throw Him Into Lake Success": multiplier = 0.5
+            
+        final_dmg = int(base_dmg * multiplier)
+        extra_dmg = final_dmg - base_dmg # 少取一次整數免得壞掉，嘻嘻嘻嘻嘻
+        
+        self.enemy_hp -= final_dmg
+        if self.enemy_type == "P.H. Chou":
+            if attack_name == "Course Evaluation Survey":
+                self.logger.log(f"You gave a negative feedback in course evalution survey!")
+                self.logger.log(f"P.H. Chou is crowded! dealt {base_dmg} + {extra_dmg} damages!")
+            elif attack_name == "Throw Him Into Lake Success":
+                self.logger.log("You throw him into Lake Success!")
+                self.logger.log(f"He just swim in the pool happily. Dealt {base_dmg} + {extra_dmg} damages!")            
+            else:
+                self.logger.log("You choose to ask for CheatGPT in Midterm Exam!")
+                self.logger.log(f"P.H. Chou didn't notice it since he didn't come to lab. dealt {base_dmg} + {extra_dmg} damages!")               
+        elif self.enemy_type == "tp6ru4z":
+            if attack_name == "Throw Him Into Lake Success":
+                self.logger.log("You throw the academic excellence award owner into Lake Success!")
+                self.logger.log(f"Ewww, the water doesn't smell good. Dealt {base_dmg} + {extra_dmg} damages!")
+            elif attack_name == "Course Evaluation Survey":
+                self.logger.log("You gave a negative feedback in course evalution survey!")
+                self.logger.log(f"While he's neither a professor nor a TA. Dealt {base_dmg} + {extra_dmg} damages!")              
+            else:
+                self.logger.log("You choose to ask for CheatGPT in Midterm Exam")
+                self.logger.log(f"He always got 10/10 on every lab, so it's impossible for you to beat him. Dealt {base_dmg} + {extra_dmg} damages!")
+        elif self.enemy_type == "Bill Louis":
+            if attack_name == "Ask for CheatGPT in Midterm Exam":
+                self.logger.log("You choose to ask for CheatGPT in Midterm Exam")
+                self.logger.log(f"This is a real headache for him. Dealt {base_dmg} + {extra_dmg} damages!")
+            elif attack_name == "Throw Him Into Lake Success":
+                self.logger.log("You throw him into Lake Success!")
+                self.logger.log(f"It seems that he doesn't care about it. Dealt {base_dmg} + {extra_dmg} damages!")                
+            else:
+                self.logger.log("You gave a negative feedback in course evalution survey!")
+                self.logger.log(f"While obviously he's not the one to be blamed. Dealt {base_dmg} + {extra_dmg} damages!") 
+
         sound_manager.play_sound("bonk.mp3")
         
         if self.check_battle_end(): return
@@ -139,6 +300,7 @@ class BattleScene(Scene):
 
     def roulette(self):
         if self.is_battle_over: return
+        self.show_attack_menu = False
         
         self.logger.log("Spinning the cylinder...")
         chance = random.random()
@@ -156,6 +318,7 @@ class BattleScene(Scene):
 
     def daydream(self):
         if self.is_battle_over: return
+        self.show_attack_menu = False
         
         sound_manager.play_sound("daydream.wav")
         self.logger.log("You daydreamed... Nothing happened")
@@ -164,7 +327,63 @@ class BattleScene(Scene):
 
     def use_item(self):
         if self.is_battle_over: return
-        self.logger.log("Great Proposal! While I haven't actually done it yet :D")
+        self.show_attack_menu = False
+        self.show_bag_menu = not self.show_bag_menu
+        self._init_bag_buttons()
+
+    # 初始化能使用的 Items 的按鈕
+    def _init_bag_buttons(self):
+        self.bag_item_buttons = []
+        bag = self.game_manager.bag
+        start_x = (GameSettings.SCREEN_WIDTH // 2) - 150
+        start_y = 150
+        line_height = 50
+        
+        if not bag.items:
+            return
+
+        for i, item in enumerate(bag.items):
+            item_name = item['name']
+            if item_name in ["Heal potion", "Strength Potion", "Defense Potion"]:
+                btn = Button(
+                    img_path="UI/raw/UI_Flat_Button01a_3.png",
+                    img_hovered_path=None,
+                    x=start_x + 300,
+                    y=start_y + i * line_height - 10,
+                    width=80, height=40,
+                    on_click=lambda idx=i, name=item_name: self._use_bag_item(idx, name)
+                )
+                self.bag_item_buttons.append((btn, "Use"))
+            
+    # 使用背包物品
+    def _use_bag_item(self, index: int, name: str):
+        bag = self.game_manager.bag
+        if index >= len(bag.items): return
+        
+        item = bag.items[index]
+        
+        if name == "Heal potion":
+            recover = 50
+            self.player_hp = min(self.player_max_hp, self.player_hp + recover)
+            self.logger.log(f"Used Heal potion. Recovered {recover} HP!")
+            
+        elif name == "Strength Potion":
+            self.strength_buff = True
+            self.logger.log("Used Strength Potion! Next attack damage x2.5!")
+            
+        elif name == "Defense Potion":
+            self.defense_buff = True
+            self.logger.log("Used Defense Potion! Next damage taken x0.25!")
+
+        # 還有剩的話就 -1，沒剩的話整個踢出去，等等 update 就不見了
+        if item['count'] > 1:
+            item['count'] -= 1
+        else:
+            bag.items.pop(index)
+            
+        sound_manager.play_sound("eating.wav")
+        self.show_bag_menu = False
+        self.enemy_turn()
 
     def run(self):
         self._mocking_coward()
@@ -190,15 +409,50 @@ class BattleScene(Scene):
         sys.exit()
 
     def enemy_turn(self):
-        enemy_dmg = random.randint(10, 25)
-        self.player_hp -= enemy_dmg
-        chance = random.random()
-        if chance < 0.333:
-            self.logger.log(f"Aeeey we'll have a pop quiz! Took {enemy_dmg} damage.")
-        elif chance < 0.666:
-            self.logger.log(f"Mary had a little lamb! Recieved {enemy_dmg} damage.")
-        else:
-            self.logger.log(f"Ch.16 is functions while ch.12 is recursion! Recieved {enemy_dmg} damage.")
+        enemy_dmg = 0
+        dialogue = "..."
+        
+        if self.enemy_type == "P.H. Chou":
+            chance = random.random()
+            if chance < 0.333:
+                enemy_dmg = random.randint(10, 25)
+                dialogue = "Aeeey we'll have a pop quiz"
+            elif chance < 0.666:
+                enemy_dmg = random.randint(10, 25)
+                dialogue = "Mary had a little lamb!"
+            else:
+                enemy_dmg = random.randint(10, 25)
+                dialogue = "Ch.16 is functions while ch.12 is recursion"
+
+        elif self.enemy_type == "tp6ru4z":
+            chance = random.random()
+            if chance < 0.333:
+                enemy_dmg = random.randint(99999, 100000)
+                dialogue = "\"I'm about to fail i2P\", emotional damage"
+            else:
+                enemy_dmg = random.randint(10, 25)
+                dialogue = "Kabi Kabi" # [填空]
+
+        elif self.enemy_type == "Bill Louis":
+            chance = random.random()
+            if chance < 0.2:
+                enemy_dmg = random.randint(30, 50)
+                dialogue = "He gave you 0 points on checkpoint.3"
+            elif chance < 0.6:
+                enemy_dmg = 0
+                dialogue = "Oops, he forget the English of he's skill name"
+            else:
+                enemy_dmg = random.randint(10, 25)
+                dialogue = "He designed a lab problem that no one knows the answer"
+
+        multiplier = 1.0
+        if self.defense_buff:
+            multiplier = 0.25
+            self.defense_buff = False
+
+        self.player_hp -= int(enemy_dmg * multiplier)
+        self.logger.log(f"{dialogue}! took {enemy_dmg}*{multiplier}={int(enemy_dmg * multiplier)} damage from {self.enemy_type}")
+        
         self.check_battle_end()
 
     def check_battle_end(self) -> bool:
@@ -225,8 +479,22 @@ class BattleScene(Scene):
                 scene_manager.change_scene("game")
             return
 
-        for btn, _ in self.buttons: # 後面只是一個完全不重要的提示字
-            btn.update(dt)
+        # 如果選單打開，更新選單按鈕
+        if self.show_attack_menu:
+            for btn, _ in self.attack_menu_buttons:
+                btn.update(dt)
+
+        # 或是更新背包裡面的按鈕
+        elif self.show_bag_menu:
+            for btn, _ in self.bag_item_buttons:
+                btn.update(dt)
+        else:
+            # 否則更新原本的按鈕
+            for btn, _ in self.buttons: # 後面只是一個完全不重要的提示字
+                btn.update(dt)
+
+            # 靠北我剛剛這裡多加一個 tab 花了十分鐘 debug
+            self.evolute_button.update(dt)
 
     @override
     def draw(self, screen: pg.Surface):
@@ -235,7 +503,7 @@ class BattleScene(Scene):
 
         
         # enemy
-        img_rect = self.enemy_img.get_rect(center=(GameSettings.SCREEN_WIDTH * 7 // 8, 100))
+        img_rect = self.enemy_img.get_rect(center=(GameSettings.SCREEN_WIDTH * 7 // 8, 140))
         screen.blit(self.enemy_img, img_rect)
         enemy_hp_text = f"Enemy HP: {self.enemy_hp}/{self.enemy_max_hp}"
         enemy_hp_surf = self.font.render(enemy_hp_text, True, (255, 50, 50))
@@ -258,4 +526,47 @@ class BattleScene(Scene):
                 screen.blit(lbl_surf, (start_x, start_y))
                 counter -= 1
 
+        # Evolute Him 按鈕
+        self.evolute_button.draw(screen)
+        evo_text = self.font.render("Evolute Him", True, (0, 0, 0)) # 用黑色字
+        screen.blit(evo_text, (self.evolute_button.hitbox.centerx - evo_text.get_width()//2, self.evolute_button.hitbox.centery - evo_text.get_height()//2))
+
+        # 攻擊選單 Overlay
+        if self.show_attack_menu:
+            s = pg.Surface((GameSettings.SCREEN_WIDTH, GameSettings.SCREEN_HEIGHT), pg.SRCALPHA)
+            s.fill((0, 0, 0, 150))
+            screen.blit(s, (0, 0))
             
+            for btn, button_name in self.attack_menu_buttons:
+                btn.draw(screen)
+                lbl_surf = self.font.render(button_name, True, (0, 0, 0))
+                screen.blit(lbl_surf, (btn.hitbox.centerx - lbl_surf.get_width()//2, btn.hitbox.centery - lbl_surf.get_height()//2))
+
+        # 背包選單 Overlay
+        if self.show_bag_menu:
+            s = pg.Surface((GameSettings.SCREEN_WIDTH, GameSettings.SCREEN_HEIGHT), pg.SRCALPHA)
+            s.fill((0, 0, 0, 150))
+            screen.blit(s, (0, 0))
+            
+            title = self.font.render("--- Items in Bag ---", True, (255, 255, 0))
+            screen.blit(title, (GameSettings.SCREEN_WIDTH // 2 - 100, 100))
+            
+            bag = self.game_manager.bag
+            start_x = (GameSettings.SCREEN_WIDTH // 2) - 150
+            start_y = 150
+            line_height = 50
+            
+            if not bag.items:
+                empty = self.font.render("How poor are you? Your Bag is EMPTY!!!", True, (200, 200, 200))
+                screen.blit(empty, (start_x, start_y))
+            else:
+                for i, item in enumerate(bag.items):
+                    text = f"{item['name']} x {item['count']}"
+                    txt_surf = self.font.render(text, True, (255, 255, 255))
+                    screen.blit(txt_surf, (start_x, start_y + i * line_height))
+            
+            # 畫 Use 按鈕
+            for btn, label in self.bag_item_buttons:
+                btn.draw(screen)
+                l_surf = self.font.render(label, True, (0, 0, 0))
+                screen.blit(l_surf, (btn.hitbox.centerx - l_surf.get_width()//2, btn.hitbox.centery - l_surf.get_height()//2))
